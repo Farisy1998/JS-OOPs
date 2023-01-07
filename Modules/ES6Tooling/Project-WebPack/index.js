b8a8138605a9efb6804bb2fb468ad58737442c63
Farisy1998 <salmanulfarisyk123@gmail.com>
2023-01-07 15:56:38 +0530

    changes committed

--- Modules/ES6Tooling/Project-WebPack/index.js
+++
@@ -1,4 +0,0 @@
-import { Circle } from './Circle.js';
-
-const c = new Circle(10);
-c.draw();
---
+++ Modules/ES6Tooling/Project-WebPack/src/index.js
@@ -0,0 +1,7 @@
+import { Circle } from './Circle';
+
+const c = new Circle(10);
+c.draw();
+
+console.log('Changed');
+console.log("Changed again");
